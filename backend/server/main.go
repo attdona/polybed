789aebe12aa50379ba28a5cffcf3f5a8c135d280
mfabio <fabio.moser@gmail.com>
2016-04-13 15:13:54 +0200

    add context variable

--- backend/server/main.go
+++ backend/server/main.go
@@ -14,7 +14,7 @@ func main() {
 	api := rest.NewApi()
 	api.Use(rest.DefaultDevStack...)
 	router, err := rest.MakeRouter(
-		rest.Get("/http/:clientId", HTTPMeasures),
+		rest.Get("/:context/:clientId", HTTPMeasures),
 	)
 	if err != nil {
 		log.Fatal(err)
@@ -25,9 +25,10 @@ func main() {
 
 // HTTPMeasures get http traffic
 func HTTPMeasures(w rest.ResponseWriter, r *rest.Request) {
+	ctx := r.PathParam("context")
 	id := r.PathParam("clientId")
 	fmt.Println("HITTED")
-	measures := backend.AllTraffic(id, "http")
+	measures := backend.AllTraffic(id, ctx)
 	fmt.Println("measures: ", measures)
 	w.WriteJson(&measures)
 }
